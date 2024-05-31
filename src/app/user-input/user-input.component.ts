6a9bef6f8a0425fb90b14139b0006f90c380c4a2
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-05-31 12:04:46 +0500

    worked in user-input component and worked by onSubmit and two way binding ngModel

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-user-input',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
+
+  onSubmit() {
+    const data = {
+      initialInvestment: this.enteredInitialInvestment,
+      annualInvestment: this.enteredAnnualInvestment,
+      expectedReturn: this.enteredExpectedReturn,
+      duration: this.enteredDuration,
+    };
+    console.log('data', data);
+  }
+}
