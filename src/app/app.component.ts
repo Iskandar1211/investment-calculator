d281138bf2e1513377d61cb032502e1fb2aea54d
DESKTOP-7K7V7QV\Lenovo <alexawp121109@gmail.com>
2024-05-31 15:51:33 +0500

    added calculateInvestmentResults in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
-import { UserInputComponent } from "./user-input/user-input.component";
+import { UserInputComponent } from './user-input/user-input.component';
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    imports: [HeaderComponent, UserInputComponent]
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults({
+    initialInvestment,
+    duration,
+    expectedReturn,
+    annualInvestment,
+  }: {
+    initialInvestment: number;
+    duration: number;
+    expectedReturn: number;
+    annualInvestment: number;
+  }) {
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    return annualData;
+  }
+}
